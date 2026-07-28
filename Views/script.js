eff57962460b98cb478cd2a0690b5d5f2d28190e
afsanaashaa <afsanaasha502@gmail.com>
2026-07-28 12:36:23 +0600

    Added Division feature

--- Views/script.js
+++ Views/script.js
@@ -30,6 +30,40 @@ async function addition() {
         = data.result;
 }
 
+async function division() {
+  const response = await fetch(
+    '/api/division',
+    {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify({
+        firstNumber: Number(
+          document.getElementById('div1').value,
+        ),
+
+        secondNumber: Number(
+          document.getElementById('div2').value,
+        ),
+
+        thirdNumber: document.getElementById('div3').value
+          ? Number(
+              document.getElementById('div3').value,
+            )
+          : null,
+      }),
+    },
+  );
+
+  const data = await response.json();
+
+  document.getElementById('divisionResult').innerHTML
+        = data.result;
+}
+
 async function prime() {
   const response = await fetch(
     '/api/prime',
