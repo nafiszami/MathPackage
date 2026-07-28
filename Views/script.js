fa1823e5f1730bfbbedc6219692b84e849d54adc
nafiszami <nafis.zami@gmail.com>
2026-07-28 12:26:03 +0600

    multiplication

--- Views/script.js
+++ Views/script.js
@@ -30,6 +30,39 @@ async function addition() {
         = data.result;
 }
 
+
+async function multiplication() {
+  const response = await fetch(
+    '/api/multiplication',
+    {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify({
+        firstNumber: Number(
+          document.getElementById('mul1').value,
+        ),
+
+        secondNumber: Number(
+          document.getElementById('mul2').value,
+        ),
+
+        thirdNumber: Number(
+          document.getElementById('mul3').value,
+        ),
+      }),
+    },
+  );
+
+  const data = await response.json();
+
+  document.getElementById('mulResult').innerHTML
+        = data.result;
+}
+
 async function prime() {
   const response = await fetch(
     '/api/prime',
