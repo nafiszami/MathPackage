b460e16a3aa63a2df15df148321a528f9e683e49
Tamjidul-Islam <tamjidtj6@gmail.com>
2026-07-28 12:21:36 +0600

    feat: add Subtraction module

--- Views/script.js
+++ Views/script.js
@@ -30,6 +30,40 @@ async function addition() {
         = data.result;
 }
 
+async function subtraction() {
+  const response = await fetch(
+    '/api/subtraction',
+    {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify({
+        firstNumber: Number(
+          document.getElementById('sub1').value,
+        ),
+
+        secondNumber: Number(
+          document.getElementById('sub2').value,
+        ),
+
+        thirdNumber: Number(
+          document.getElementById('sub3').value,
+        ),
+      }),
+    },
+  );
+
+  const data = await response.json();
+
+  document.getElementById('subResult').innerHTML
+        = data.result;
+}
+
+
+
 async function prime() {
   const response = await fetch(
     '/api/prime',
